a5450f9d16c5d3175ce311db758b378c7762c7d1
Ronald van der Bergh <rhvdbergh@gmail.com>
2020-01-13 21:16:12 -0600

    change colors file to array, assign shape to color

--- src/data/colors.js
+++ src/data/colors.js
@@ -1,12 +1,12 @@
 // seven colors for seven pieces of blocks
-const colors = {
-  cyan: 'cyan',
-  yellow: 'yellow',
-  purple: 'purple',
-  green: 'green',
-  blue: 'blue',
-  red: 'red',
-  orange: 'orange'
-};
+const colors = [
+  'cyan', // S shape
+  'yellow', // Z shape
+  'purple', // T shape
+  'green', // L shape
+  'blue', // reverse L shape
+  'red', // LINE shape
+  'orange' // BLOCK shape
+];
 
 export default colors;
